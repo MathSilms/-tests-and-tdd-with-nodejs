833d8b96ff38167bf083016260c4b47cdfd15f63
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-09-29 04:33:57 -0300

    create base

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+
+export default {
+  host :'127.0.0.1',
+  username:'docker',
+  password:'docker',
+  database:'nodeAuth',
+  dialect:'postgres',
+  operatorsAliases:false,
+  logging:false,
+  define: {
+    timestamps:true,
+    underscored:true,
+    underscoredAll:true,
+  },
+
+}
